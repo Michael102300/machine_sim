212b863595e1c404825521715e94fc7ccb1ca4cb
MichaelOlivares <michaelolivares123@gmail.com>
2022-10-01 11:10:29 -0500

    feat(): add comments in rp2_sim.py in only functions

--- rp2_sim.py
+++ rp2_sim.py
@@ -1,6 +1,17 @@
 def asm_pio(*args, **kwargs):
+    """ 
+        Es un decarador padre en el cual se reciben los argumentos que se guardan en listas,
+        *args: argumentos que no tiene asignacion variable
+        **kawrgs: argumentos que tiene asignacion variable=asignacion
+    """
     def decorador(programa):
+        """ 
+            Es un decarador hijo en el cual se reciben la funcion a modificar en el parametro de programa.
+        """
         def compilador():
+            """
+                Funcion que se ejecuta en la funcion que recibida en el decorador hijo
+            """
             print("Parámetros", kwargs) 
             programa()
             return None
@@ -8,7 +19,9 @@ def asm_pio(*args, **kwargs):
     return decorador
 
 def decorador_instr(fun_inst):
+    """ Decorador de la funcion recibida en el parametro fun_inst """
     def decoracion_instr(self,*args, **kwargs):
+        """ Funcion que no hace nada, solamente llama a fun_inst y le envia todo tal cual """
         fun_inst(self,*args, **kwargs)
         return None 
     return decoracion_instr
@@ -21,6 +34,17 @@ class PIO():
 
 class StateMachine:
   def __init__(self, id_, program, freq=125000000, **kwargs):
+        """
+            Contructor de la clase StateMachine, se iniciala con estos parametros
+            id_: id de la maquina a usar
+            progra: programa a ejecutar en la clase de StateMachine
+            freq: frecuencia de la maquina de estado
+            **kwargs: argumentos en una lista   
+            Este contructor ejecutar el programa enviado en program,
+            imprime las instrucciones leidas que llegan mediante el parametro **kwargs, 
+            resetea la variable sm_inciandose, que indica el id de la maquina de estado que se esta ejecutando,
+            asigna en el arreglo de fsms en la posicion del id_ la StateMachine
+        """
         global sm_iniciandose,fsms
         sm_iniciandose=self
         #print('StateMachine.__init__',id_, program, freq, kwargs)
@@ -33,7 +57,7 @@ class StateMachine:
       
         
   def active(self, x=None):
-    '''Esta rutina simula exclisivamnte esa FSM. Sería interesante crear simulación en parlelo con otras FSM'''
+    '''Esta rutina simula exclusivamnte esa FSM. Sería interesante crear simulación en parlelo con otras FSM'''
     if x==1:
         print('Está pendiente de realizar la simulacón')
 
@@ -45,6 +69,11 @@ sm_iniciandose=None
 class nop:
     @decorador_instr
     def __init__(self,*args, **kwargs):
+        """
+            Contructor de la clase nop (con el decorador @decorador_instr), el cual realiza lo siguiente:
+            imprime el nombre de la clase que se le envia en el paramatero de self
+            selecciona la variable globar sm_iniciandose y le agrega la clase de self 
+        """
         global sm_iniciandose
         print(self.__class__.__name__)#,'nop.__init__',args,kwargs)
         sm_iniciandose.lista_instr.append(self)
@@ -52,22 +81,37 @@ class nop:
         pass
      
     def __getitem__(self,name):
+        """
+            Funcion donde se adquiere un item, aun no esta desarrollada 
+        """
         #print('nop.__getattr__',name)
         pass
         
 class set(nop):
     def __init__(self,*args, **kwargs):
+        """
+            Contructor de la clase set donde usa el init de la clase enviada mediante el parametro 
+            usando el metodo super
+        """
         super().__init__(*args, **kwargs)
         pass
    
 class wrap_target(nop):
     def __init__(self,*args, **kwargs):
-         super().__init__(*args, **kwargs)
-         pass 
+        """
+            Contructor de la clase set donde usa el init de la clase enviada mediante el parametro 
+            usando el metodo super
+        """
+        super().__init__(*args, **kwargs)
+        pass 
   
 class wrap(nop):
     def __init__(self,*args, **kwargs):
-         super().__init__(*args, **kwargs)
-         pass 
+        """
+            Contructor de la clase set donde usa el init de la clase enviada mediante el parametro 
+            usando el metodo super
+        """
+        super().__init__(*args, **kwargs)
+        pass 
          
          
